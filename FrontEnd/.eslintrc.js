da28a6cb3cd756f5ed20525e9a6fadcbeb0a184d
Yağız Bora <yaizbora@gmail.com>
2025-05-05 13:26:04 +0300

    `Update ESLint configuration to use recommended rules and React plugin`

--- FrontEnd/.eslintrc.js
+++ FrontEnd/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
