71359ead6266d432a5fc742476c0fbff10d25914
Yağız Bora <yaizbora@gmail.com>
2025-05-24 16:43:19 +0300

    Update api

---
+++ Backend/KutuphaneYonetimSistemi/Controllers/AuthorController.cs
@@ -0,0 +1,92 @@
+﻿using Dapper;
+using KutuphaneYonetimSistemi.Common;
+using KutuphaneYonetimSistemi.Models;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KutuphaneYonetimSistemi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+
+        private readonly DbHelper _dbHelper;
+
+        public AuthorController(DbHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+
+
+        [HttpGet("GetAuthor")]
+        public async Task<IActionResult> Getauthor()
+        {
+            try
+            {
+                using (var connection = _dbHelper.GetConnection())
+                {
+                    string query = "SELECT id,name_surname,biography,birthday_date FROM table_authors WHERE is_deleted = false";
+                    var result = (await connection.QueryAsync<GetAuthor>(query)).ToList();
+                    if (result.Count != 0)
+                    {
+                        return Ok(result);
+                    }
+                    else
+                    {
+                        return NotFound(ResponseHelper.NotFoundResponse(ReturnMessages.NotFound));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ResponseHelper.ExceptionResponse(ex.Message));
+            }
+        }
+
+        [HttpGet("GetAuthor/{id}")]
+        public async Task<IActionResult> Getauthorbyid(int id) 
+        {
+            try
+            {
+                using (var connection = _dbHelper.GetConnection())
+                {
+                    string query = "SELECT id,name_surname,biography,birthday_date FROM table_authors WHERE is_deleted = false AND id = @id";
+                    var result = (await connection.QueryAsync<GetAuthor>(query, new { id = id })).ToList(); 
+                    if (result.Count != 0) 
+                    {
+                        return Ok(result);
+                    }
+                    else
+                    {
+                        return NotFound(ResponseHelper.NotFoundResponse(ReturnMessages.NotFound));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ResponseHelper.ExceptionResponse(ex.Message));
+            }
+        }
+        [HttpPost("CreateAuthor")]
+        public async Task <IActionResult> CreateAuthor(CreateAuthor model)
+        {
+            try
+            {
+                using (var connection = _dbHelper.GetConnection())
+                {
+                    string query = "INSERT INTO table_authors(name_surname,biography,birthday_date,is_deleted) VALUES (@name_surname,@biography,@birthday_date,false)";
+                    var result = await connection.ExecuteAsync(query, model);
+                    return Ok(ResponseHelper.ActionResponse(ReturnMessages.RecordAdded));
+
+                }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ResponseHelper.ExceptionResponse(ex.Message));
+
+            }
+        }
+        
+    }
+}
---
+++ Backend/KutuphaneYonetimSistemi/Models/AuthorModels.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KutuphaneYonetimSistemi.Models
+{
+    public class GetAuthor
+    {
+        public int id { get; set; }
+        public string name_surname { get; set; }
+        public DateTime birthday_date { get; set; }
+        public string biography { get; set; }
+    }
+    public class CreateAuthor
+    {
+        public required string name_surname { get; set; }
+        public required DateTime birthday_date { get; set; }
+        public required string biography { get; set; }
+    }
+
+
+
+}
