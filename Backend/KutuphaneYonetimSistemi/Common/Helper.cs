c6a839d2122a919c9955483c969521b5c9231cab
Yağız Bora <yaizbora@gmail.com>
2025-07-07 04:07:14 +0300

    `Added new methods to Helper class for number validation and email validation`

--- Backend/KutuphaneYonetimSistemi/Common/Helper.cs
+++ Backend/KutuphaneYonetimSistemi/Common/Helper.cs
@@ -1,4 +1,6 @@
-﻿namespace KutuphaneYonetimSistemi.Common
+﻿using System.Text.RegularExpressions;
+
+namespace KutuphaneYonetimSistemi.Common
 {
     public class Helper
     {
@@ -14,13 +16,39 @@
                 }
                 else
                 {
-                    return (true,"");
+                    return (true, "");
                 }
             }
             catch (Exception ex)
             {
                 return (false, ex.Message);
-            };
+            }
+            ;
         }
+
+        public (bool status, string message) IsNumberIsTrue(string number)
+        {
+            try
+            {
+                if (int.TryParse(number, out _))
+                {
+                    return (true, "Sayı geçerli");
+                }
+                else
+                {
+                    return (false,"Bu bir sayı değil!");
+                }
+            }
+            catch
+            {
+                return (false,"Bi şeyler ters gitti");
+            }
+        }
+
+        public bool validatemail(string email)
+        {
+            return Regex.IsMatch(email, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+        }
+
     }
 }
