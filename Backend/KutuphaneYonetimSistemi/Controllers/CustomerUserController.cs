2e48f7712c06300079279aa723fba381fddc5c6c
Yağız Bora <yaizbora@gmail.com>
2025-07-21 20:42:52 +0300

    CreateCustomerUser created

---
+++ Backend/KutuphaneYonetimSistemi/Controllers/CustomerUserController.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using KutuphaneYonetimSistemi.Common;
+using KutuphaneYonetimSistemi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KutuphaneYonetimSistemi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerUserController : ControllerBase
+    {
+        private readonly DbHelper _dbHelper;
+
+        public CustomerUserController(DbHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+        public const int saltrounds = 12;
+
+        [HttpPost("CreateCustomerUser")]
+        public async Task<IActionResult> CreateCustomerUser(CustomerUserModels model)
+        {
+            TokenController g = new TokenController(_dbHelper);
+            var login = g.GetUserByToken(ControllerContext);
+            if (!login.Status)
+                return Unauthorized(ResponseHelper.UnAuthorizedResponse(login?.Message));
+
+                try
+                {
+                    using (var connection = _dbHelper.GetConnection())
+                    {
+
+                        string usernameisexistquery = "SELECT COUNT(*) FROM table_customer_users where username = @username";
+                        int usernameisexist = connection.QueryFirstOrDefault<int>(usernameisexistquery, model);
+                        if (usernameisexist > 0)
+                        {
+                            return BadRequest(ResponseHelper.ErrorResponse("Username is exist user couldnt create"));
+                        }
+
+
+                        string hashedpassword = model.password;
+                        string passwordHash = BCrypt.Net.BCrypt.HashPassword(hashedpassword, saltrounds);
+
+                        string query = $"INSERT INTO table_customer_users (username, hashedpassword,is_deleted,is_login,name_surname,tc_kimlik_no,birthday_date,phone_number,eposta) VALUES (@username, @passwordHash,false,false,@name_surname,@tc_kimlik_no,@birthday_date,@phone_number,@eposta)";
+                        var result = await connection.ExecuteAsync(query, new { username = model.username, passwordHash = passwordHash,name_surname = model.name_surname, tc_kimlik_no = model.tc_kimlik_no, birthday_date = model.birthday_date,phone_number = model.phone_number,eposta = model.eposta });
+                        return Ok(ResponseHelper.ResponseSuccesfully<object>("Customer User created Successfully"));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ResponseHelper.ExceptionResponse(ex.Message));
+                }
+        }
+    } 
+}
+
---
+++ Backend/KutuphaneYonetimSistemi/Models/CustomerUserModels.cs
@@ -0,0 +1,13 @@
+﻿namespace KutuphaneYonetimSistemi.Models
+{
+    public class CustomerUserModels
+    {
+        public required string tc_kimlik_no { get; set; }
+        public required string username { get; set; }
+        public required string password { get; set; }
+        public required string name_surname { get; set; }
+        public required DateTime birthday_date { get; set; }
+        public required decimal phone_number { get; set; }
+        public required string eposta { get; set; }
+    }
+}
