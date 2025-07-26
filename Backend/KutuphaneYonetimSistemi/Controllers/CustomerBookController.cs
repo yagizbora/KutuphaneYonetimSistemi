839f30bdf9ff2c7ba51807526aa17e086c4ee474
Yağız Bora <yaizbora@gmail.com>
2025-07-27 01:17:41 +0300

    `Added Dapper and CustomerBookModels to CustomerBookController, modified CustomerBookList method to use Dapper for database query.`

--- Backend/KutuphaneYonetimSistemi/Controllers/CustomerBookController.cs
+++ Backend/KutuphaneYonetimSistemi/Controllers/CustomerBookController.cs
@@ -1,4 +1,6 @@
-﻿using KutuphaneYonetimSistemi.Common;
+﻿using Dapper;
+using KutuphaneYonetimSistemi.Common;
+using KutuphaneYonetimSistemi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,7 +20,31 @@ namespace KutuphaneYonetimSistemi.Controllers
         [HttpGet("CustomerBookList")]
         public async Task<IActionResult> CustomerBookList()
         {
-            return Ok(); // Will be create
+            CustomerTokenController g = new(_dbHelper);
+            var login = g.GetUserByToken(ControllerContext);
+            if (!login.Status)
+                return Unauthorized(ResponseHelper.UnAuthorizedResponse(login?.Message));
+            try
+            {
+                using(var connection = _dbHelper.GetConnection())
+                {
+                    string sql = "SELECT tk.id, tk.kitap_adi, tk.durum,lb.library_name,lb.location,au.name_surname as author_name " +
+                                 "FROM table_kitaplar tk " +
+                                 "JOIN table_libraries lb ON lb.id = tk.library_id " +
+                                 "JOIN table_authors au ON au.id = tk.author_id " +
+                                 "WHERE tk.is_deleted = false AND tk.durum = true;";
+                    var result = await connection.QueryAsync<CustomerBookModels>(sql);
+                    if (result == null || !result.Any())
+                    {
+                        return NotFound(ResponseHelper.NotFoundResponse("No books found."));
+                    }
+                    return Ok(ResponseHelper.OkResponse("Books retrieved successfully.", result));
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ResponseHelper.ExceptionResponse(ex.Message));
+            }
         }
     }
 }
---
+++ Backend/KutuphaneYonetimSistemi/Models/CustomerBookModels.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace KutuphaneYonetimSistemi.Models
+{
+    public class CustomerBookModels
+    {
+        [JsonPropertyOrder(1)]
+        public int? id { get; set; }
+        [JsonPropertyOrder(2)]
+        public string? kitap_adi { get; set; }
+        [JsonPropertyOrder(3)]
+        public bool? durum { get; set; }
+        [JsonPropertyOrder(4)]
+        public string? author_name { get; set; }
+        [JsonPropertyOrder(5)]
+        public string? library_name { get; set; }
+        [JsonPropertyOrder(6)]
+        public string? location { get; set; }
+    }
+}
